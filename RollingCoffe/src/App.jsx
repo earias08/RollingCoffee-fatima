23572fa3a48a52ffc163e2398e9e1786afb9a2d0
fatiperea <fatimaperea3pppp0@gmail.com>
2024-02-06 23:30:28 -0300

    completo nav con logo

--- RollingCoffe/src/App.jsx
+++ RollingCoffe/src/App.jsx
@@ -1,25 +1,20 @@
-import { useState } from 'react'
-import './App.css'
-import Menu from './components/Menu'
-import Inicio from './components/pages/Inicio'
-import Footer from './components/Footer'
+import { useState } from "react";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Menu from "./components/Menu";
+import Inicio from "./components/pages/Inicio";
+import Footer from "./components/Footer";
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
-    <> 
-    <Menu> 
-    </Menu>
-    <Inicio> 
-    </Inicio>
-    <Footer> 
-    </Footer>
-
-
-
+    <>
+      <Menu></Menu>
+      <Inicio></Inicio>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- RollingCoffe/src/components/Menu.jsx
+++ RollingCoffe/src/components/Menu.jsx
@@ -1,30 +1,21 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-
-
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from '../assets/Coffee_Logo.png'
 const Menu = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img src={logo} alt="logo" className="img-fluid" width={200} />
+
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#link">Admin</Nav.Link>
+            <Nav.Link href="#home">Login</Nav.Link>
+            <Nav.Link href="#link">Registro</Nav.Link>
+
           </Nav>
         </Navbar.Collapse>
       </Container>
